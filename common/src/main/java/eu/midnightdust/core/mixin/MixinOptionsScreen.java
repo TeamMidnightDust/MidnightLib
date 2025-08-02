6cc5a484a5e42e67bae861322e06f4d33ec896cd
maloryware <mal.larsson.reyes@gmail.com>
2025-08-02 06:59:44 +0100

    progress towards 1.20.1 backport

--- common/src/main/java/eu/midnightdust/core/mixin/MixinOptionsScreen.java
+++ common/src/main/java/eu/midnightdust/core/mixin/MixinOptionsScreen.java
@@ -1,52 +1,30 @@
 package eu.midnightdust.core.mixin;
 
+import eu.midnightdust.core.config.MidnightLibConfig;
 import eu.midnightdust.core.screen.MidnightConfigOverviewScreen;
+import eu.midnightdust.lib.util.PlatformFunctions;
+import eu.midnightdust.lib.util.screen.TexturedOverlayButtonWidget;
 import net.minecraft.client.gui.screen.Screen;
 import net.minecraft.client.gui.screen.option.OptionsScreen;
-import net.minecraft.client.gui.widget.TextIconButtonWidget;
-import net.minecraft.client.gui.widget.ThreePartsLayoutWidget;
 import net.minecraft.text.Text;
 import net.minecraft.util.Identifier;
-import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Shadow;
-import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 import java.util.Objects;
 
-import static eu.midnightdust.core.MidnightLib.MOD_ID;
-import static eu.midnightdust.core.config.MidnightLibConfig.shouldShowButton;
-
 @Mixin(OptionsScreen.class)
-public abstract class MixinOptionsScreen extends Screen {
-    @Shadow @Final private ThreePartsLayoutWidget layout;
-    @Unique TextIconButtonWidget midnightlib$button = TextIconButtonWidget.builder(Text.translatable("midnightlib.overview.title"), (
-            buttonWidget) -> Objects.requireNonNull(client).setScreen(new MidnightConfigOverviewScreen(this)), true)
-            .texture(Identifier.of(MOD_ID,"icon/"+MOD_ID), 16, 16).dimension(20, 20).build();
-
-    private MixinOptionsScreen(Text title) {super(title);}
-
-    @Inject(at = @At("HEAD"), method = "init")
-    public void midnightlib$onInit(CallbackInfo ci) {
-        if (shouldShowButton()) {
-            this.midnightlib$setButtonPos();
-            this.addDrawableChild(midnightlib$button);
-        }
+public class MixinOptionsScreen extends Screen {
+    private static final Identifier MIDNIGHTLIB_ICON_TEXTURE = new Identifier("midnightlib","textures/gui/midnightlib_button.png");
+    protected MixinOptionsScreen(Text title) {
+        super(title);
     }
 
-
-
-    @Inject(at = @At("TAIL"), method = "initTabNavigation")
-    public void midnightlib$onResize(CallbackInfo ci) {
-        if (shouldShowButton()) this.midnightlib$setButtonPos();
-    }
-
-
-    @Unique
-    public void midnightlib$setButtonPos() {
-        midnightlib$button.setPosition(layout.getWidth() / 2  + 158, layout.getY() + layout.getFooterHeight() - 4);
+    @Inject(at = @At("HEAD"),method = "init")
+    private void midnightlib$init(CallbackInfo ci) {
+        if (MidnightLibConfig.config_screen_list.equals(MidnightLibConfig.ConfigButton.TRUE) || (MidnightLibConfig.config_screen_list.equals(MidnightLibConfig.ConfigButton.MODMENU) && !PlatformFunctions.isModLoaded("modmenu")))
+            this.addDrawableChild(new TexturedOverlayButtonWidget(this.width / 2 + 158, this.height / 6 - 12, 20, 20, 0, 0, 20, MIDNIGHTLIB_ICON_TEXTURE, 32, 64, (buttonWidget) -> Objects.requireNonNull(client).setScreen(new MidnightConfigOverviewScreen(this)), Text.translatable("midnightlib.overview.title")));
     }
 }
--- common/src/main/java/eu/midnightdust/lib/config/MidnightConfig.java
+++ common/src/main/java/eu/midnightdust/lib/config/MidnightConfig.java
@@ -3,6 +3,7 @@ package eu.midnightdust.lib.config;
 import com.google.common.collect.Lists;
 import com.google.gson.*; import com.google.gson.stream.*;
 import com.mojang.blaze3d.systems.RenderSystem;
+import com.mojang.serialization.DataResult;
 import eu.midnightdust.lib.util.PlatformFunctions;
 import net.fabricmc.api.EnvType; import net.fabricmc.api.Environment;
 import net.minecraft.client.MinecraftClient; import net.minecraft.client.font.TextRenderer;
@@ -97,8 +98,10 @@ public abstract class MidnightConfig {
                 if (!condition.requiredModId().isEmpty() && !PlatformFunctions.isModLoaded(condition.requiredModId()))
                     this.conditionsMet = false;
                 String requiredOption = condition.requiredOption().contains(":") ? condition.requiredOption() : (this.modid + ":" + condition.requiredOption());
-                if (entries.get(requiredOption) instanceof EntryInfo info)
+                if (entries.get(requiredOption) != null) {
+                    EntryInfo info = entries.get(requiredOption);
                     this.conditionsMet &= List.of(condition.requiredValue()).contains(info.tempValue);
+                }
                 if (!this.conditionsMet) break;
             }
             if (prevConditionState != this.conditionsMet) reloadScreen = true;
@@ -195,8 +198,7 @@ public abstract class MidnightConfig {
         info.function = (BiFunction<TextFieldWidget, ButtonWidget, Predicate<String>>) (t, b) -> s -> {
             s = s.trim();
             if (!(s.isEmpty() || !isNumber || pattern.matcher(s).matches()) ||
-                    (info.dataType == Identifier.class && Identifier.validate(s).isError())) return false;
-
+                    (info.dataType == Identifier.class && Identifier.validate(s).equals(DataResult.success(new Identifier(s))))) return false; // inline substitution for "isError"
             Number value = 0; boolean inLimits = false; info.error = null;
             if (!(isNumber && s.isEmpty()) && !s.equals("-") && !s.equals(".")) {
                 try { value = f.apply(s); } catch(NumberFormatException e){ return false; }
@@ -300,8 +302,10 @@ public abstract class MidnightConfig {
             if (this.list != null) {
                 for (ButtonEntry entry : this.list.children()) {
                     if (entry.buttons != null && entry.buttons.size() > 1 && entry.info.field != null) {
-                        if (entry.buttons.get(0) instanceof ClickableWidget widget)
+                        if (entry.buttons.get(0) != null) {
+                            ClickableWidget widget = entry.buttons.get(0);
                             if (widget.isFocused() || widget.isHovered()) widget.setTooltip(entry.info.getTooltip(true));
+                        }
                         if (entry.buttons.get(1) instanceof ButtonWidget button)
                             button.active = !Objects.equals(String.valueOf(entry.info.value), String.valueOf(entry.info.defaultValue)) && entry.info.conditionsMet;
         }}}}
@@ -333,7 +337,7 @@ public abstract class MidnightConfig {
                 Objects.requireNonNull(client).setScreen(parent);
             }).dimensions(this.width / 2 + 4, this.height - 26, 150, 20).build());
 
-            this.list = new MidnightConfigListWidget(this.client, this.width, this.height - 57, 24, 25);
+            this.list = new MidnightConfigListWidget(this.client, this.width, this.height, this.height - 57, 24, 25);
             this.addSelectableChild(this.list); fillList();
             if (tabs.size() > 1) list.renderHeaderSeparator = false;
         }
@@ -353,11 +357,11 @@ public abstract class MidnightConfig {
                 }
                 if (info.modid.equals(modid) && (info.tab == null || info.tab == tabManager.getCurrentTab())) {
                     Text name = Objects.requireNonNullElseGet(info.name, () -> Text.translatable(translationPrefix + info.fieldName));
-                    TextIconButtonWidget resetButton = TextIconButtonWidget.builder(Text.translatable("controls.reset"), (button -> {
+                    IconButtonWidget resetButton = IconButtonWidget.builder(Text.translatable("controls.reset"), Identifier.of("midnightlib","icon/reset"), (button -> {
                         info.value = info.defaultValue; info.listIndex = 0;
                         info.tempValue = info.toTemporaryValue();
                         updateList();
-                    }), true).texture(Identifier.of("midnightlib","icon/reset"), 12, 12).dimension(20, 20).build();
+                    }) ).build();
                     resetButton.setPosition(width - 205 + 150 + 25, 0);
 
                     if (info.function != null) {
@@ -405,7 +409,7 @@ public abstract class MidnightConfig {
                             } catch (Exception ignored) {}
                             info.actionButton = colorButton;
                         } else if (e.selectionMode() > -1) {
-                            ButtonWidget explorerButton = TextIconButtonWidget.builder(Text.empty(),
+                            ButtonWidget explorerButton = IconButtonWidget.builder(Text.empty(), Identifier.of("midnightlib", "icon/explorer"),
                                     button -> new Thread(() -> {
                                         JFileChooser fileChooser = new JFileChooser(info.tempValue);
                                         fileChooser.setFileSelectionMode(e.selectionMode()); fileChooser.setDialogType(e.fileChooserType());
@@ -417,8 +421,8 @@ public abstract class MidnightConfig {
                                             info.setValue(fileChooser.getSelectedFile().getAbsolutePath());
                                             updateList();
                                         }
-                                    }).start(), true
-                            ).texture(Identifier.of("midnightlib", "icon/explorer"), 12, 12).dimension(20, 20).build();
+                                    }).start()
+                            ).build();
                             explorerButton.setPosition(width - 185, 0);
                             info.actionButton = explorerButton;
                         }
@@ -449,9 +453,10 @@ public abstract class MidnightConfig {
     @Environment(EnvType.CLIENT)
     public static class MidnightConfigListWidget extends ElementListWidget<ButtonEntry> {
         public boolean renderHeaderSeparator = true;
-        public  MidnightConfigListWidget(MinecraftClient client, int width, int height, int y, int itemHeight) { super(client, width, height, y, itemHeight); }
-        @Override public int getScrollbarX() { return this.width -7; }
+        public MidnightConfigListWidget(MinecraftClient client, int width, int height, int yStart, int y, int itemHeight) { super(client, width, height, yStart, y, itemHeight); }
+        @Override public int getScrollbarPositionX() { return this.width -7; }
 
+        /*
         @Override
         protected void drawHeaderAndFooterSeparators(DrawContext context) {
             if (renderHeaderSeparator) super.drawHeaderAndFooterSeparators(context);
@@ -459,6 +464,7 @@ public abstract class MidnightConfig {
                 context.drawTexture(this.client.world == null ? Screen.FOOTER_SEPARATOR_TEXTURE : Screen.INWORLD_FOOTER_SEPARATOR_TEXTURE, this.getX(), this.getBottom(), 0.0F, 0.0F, this.getWidth(), 2, 32, 2);
                 RenderSystem.disableBlend(); }
         }
+        */
         public void addButton(List<ClickableWidget> buttons, Text text, EntryInfo info) { this.addEntry(new ButtonEntry(buttons, text, info)); }
         public void clear() { this.clearEntries(); }
         @Override public int getRowWidth() { return 10000; }
@@ -491,7 +497,8 @@ public abstract class MidnightConfig {
                 boolean tooltipVisible = mouseX >= title.getX() && mouseX < title.getWidth() + title.getX() && mouseY >= title.getY() && mouseY < title.getHeight() + title.getY();
                 if (tooltipVisible && title.getTooltip() != null) context.drawOrderedTooltip(textRenderer, title.getTooltip().getLines(MinecraftClient.getInstance()), mouseX, mouseY);
 
-                if (info.entry != null && !this.buttons.isEmpty() && this.buttons.getFirst() instanceof ClickableWidget widget) {
+                if (info.entry != null && !this.buttons.isEmpty() && this.buttons.get(0) != null) {
+                    ClickableWidget widget = this.buttons.get(0);
                     int idMode = this.info.entry.idMode();
                     if (idMode != -1) context.drawItem(idMode == 0 ? Registries.ITEM.get(Identifier.tryParse(this.info.tempValue)).getDefaultStack() : Registries.BLOCK.get(Identifier.tryParse(this.info.tempValue)).asItem().getDefaultStack(), widget.getX() + widget.getWidth() - 18, y + 2);
                 }
@@ -501,7 +508,7 @@ public abstract class MidnightConfig {
         @Override
         public boolean mouseClicked(double mouseX, double mouseY, int button) {
             if (this.info != null && this.info.comment != null && !this.info.comment.url().isBlank())
-                ConfirmLinkScreen.open(MinecraftClient.getInstance().currentScreen, this.info.comment.url(), true);
+                ConfirmLinkScreen.open(this.info.comment.url(), MinecraftClient.getInstance().currentScreen, true);
             return super.mouseClicked(mouseX, mouseY, button);
         }
 
